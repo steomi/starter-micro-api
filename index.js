2b3f1399ea19a231e7b973b8f220d194c1a1cb0b
steomi <75536223+steomi@users.noreply.github.com>
2023-12-19 22:28:19 +0300

    1

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
-import * as fs from 'node:fs';
-import * as http from 'node:http';
-import * as path from 'node:path';
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
 
-const PORT = 3000;
+http.createServer(function (request, response) {
+    console.log("request ", request.url);
 
-const MIME_TYPES = {
-    default: 'application/octet-stream',
-    html: 'text/html; charset=UTF-8',
-    js: 'application/javascript',
-    css: 'text/css',
-    png: 'image/png',
-    jpg: 'image/jpg',
-    gif: 'image/gif',
-    ico: 'image/x-icon',
-    svg: 'image/svg+xml',
-};
+    var filePath = "." + request.url;
+    if (filePath == "./") {
+      filePath = "./index.html";
+    }
 
-const STATIC_PATH = path.join(process.cwd(), './static');
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+      ".html": "text/html",
+      ".js": "text/javascript",
+      ".css": "text/css",
+      ".json": "application/json",
+      ".png": "image/png",
+      ".jpg": "image/jpg",
+      ".gif": "image/gif",
+      ".svg": "image/svg+xml",
+      ".wav": "audio/wav",
+      ".mp4": "video/mp4",
+      ".woff": "application/font-woff",
+      ".ttf": "application/font-ttf",
+      ".eot": "application/vnd.ms-fontobject",
+      ".otf": "application/font-otf",
+      ".wasm": "application/wasm",
+    };
 
-const toBool = [() => true, () => false];
+    var contentType = mimeTypes[extname] || "application/octet-stream";
 
-const prepareFile = async (url) => {
-    const paths = [STATIC_PATH, url];
-    if (url.endsWith('/')) paths.push('index.html');
-    const filePath = path.join(...paths);
-    const pathTraversal = !filePath.startsWith(STATIC_PATH);
-    const exists = await fs.promises
-        .access(filePath)
-        .then(...toBool);
-    const found = !pathTraversal && exists;
-    const streamPath = found
-        ? filePath
-        : STATIC_PATH + '/404.html';
-    const ext = path
-        .extname(streamPath)
-        .substring(1)
-        .toLowerCase();
-    const stream = fs.createReadStream(streamPath);
-    return { found, ext, stream };
-};
+    fs.readFile(filePath, function (error, content) {
+      if (error) {
+        if (error.code == "ENOENT") {
+          fs.readFile("./404.html", function (error, content) {
+            response.writeHead(404, { "Content-Type": "text/html" });
+            response.end(content, "utf-8");
+          });
+        } else {
+          response.writeHead(500);
+          response.end(
+            "Sorry, check with the site admin for error: " +
+              error.code +
+              " ..\n",
+          );
+        }
+      } else {
+        response.writeHead(200, { "Content-Type": contentType });
+        response.end(content, "utf-8");
+      }
+    });
+  })
+  .listen(3000);
+console.log("Server running at http://127.0.0.1:8125/");
 
-http.createServer(async (req, res) => {
-    const file = await prepareFile(req.url);
-    const statusCode = file.found ? 200 : 404;
-    const mimeType =
-        MIME_TYPES[file.ext] || MIME_TYPES.default;
-    res.writeHead(statusCode, { 'Content-Type': mimeType });
-    file.stream.pipe(res);
-    console.log(`${req.method} ${req.url} ${statusCode}`);
-}).listen(PORT);
-
-console.log(`Server running at http://127.0.0.1:${PORT}/`);
 
 
 // var http = require('http');
